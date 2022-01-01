bff193005f81a2873e854594a530f1e8ad970882
Maurici0ML <alanmauriciomoraleslopez@gmail.com>
2022-01-01 11:39:01 -0600

    FEAT: delegación de eventos en botones

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ window.onload = () => {
     timer.innerText = `${mins}:0${segs}`;
 
     //Botones
+    let controles = document.getElementById("controls");
     let start = document.getElementById("start");
     let addMins = document.getElementById("addMins");
 
@@ -41,63 +42,86 @@ window.onload = () => {
     /*EVENTOS CLICK
     ---------------------------------------------------------------------------------*/
 
-    //Evento boton inicio
-    start.addEventListener("click", ()=>{
+    
+    controles.addEventListener("click", (evento)=>{+
 
-        //Condicional para determinar play y pause del interval
-        if( start.innerHTML === '\n        <i class="fas fa-2x fa-play"></i>\n    ' || start.innerHTML === '<i class="fas fa-2x fa-play"></i>' ) {
+        console.log(evento.target.className);
+        
+        //Constante que guarda el estado de los botones (-1 si no fue tocado)
+        const ControlBtn = [
+            evento.target.className.indexOf("lssMins"),
+            evento.target.className.indexOf("start"), 
+            evento.target.className.indexOf("addMins")
+        ];
+        
+        console.log(ControlBtn);
 
-            start.innerHTML = '<i class="fas fa-2x fa-pause"></i>'; //Cambiando el icno del botón a Pausar.
+        //Evento boton inicio( usando delegación de eventos )
+        if ( ControlBtn[1] !== -1 ) {
 
-            //Intervalo que lleva el tiempo
-            intervalo = setInterval( () => {
-                
-                //Cuando el contador llegue a 0...
-                if( mins === 0 && segs === 0 ) {
+            //Condicional para determinar play y pause del interval
+            if( start.innerHTML === '\n            <i class="btn-start fas fa-2x fa-play"></i>\n        ' || start.innerHTML === '<i class="btn-start fas fa-2x fa-play"></i>' ) {
+
+                start.innerHTML = '<i class="btn-start fas fa-2x fa-pause"></i>'; //Cambiando el icno del botón a Pausar.
+
+                //Intervalo que lleva el tiempo
+                intervalo = setInterval( () => {
                     
-                    clearInterval( intervalo ); //Limpiando el intervalo.
+                    //Cuando el contador llegue a 0...
+                    if( mins === 0 && segs === 0 ) {
+                        
+                        clearInterval( intervalo ); //Limpiando el intervalo.
 
-                    //Reproducciendo el sonido de final.
-                    final.play();
+                        //Reproducciendo el sonido de final.
+                        final.play();
 
-                    //Reiniciando el contador 
-                    mins = 60;
-                    segs = 0;                    
+                        //Reiniciando el contador 
+                        mins = 60;
+                        segs = 0;                    
 
-                } else if( segs === 0 ) {   //Manejo de los segundos y minutos.
+                    } else if( segs === 0 ) {   //Manejo de los segundos y minutos.
 
-                    segs = 60;
-                    mins--;
+                        segs = 60;
+                        mins--;
 
-                }
+                    }
 
-                //Restando los segundos.
-                segs--;
-                segs = segs === -1? 0:segs;
+                    //Restando los segundos.
+                    segs--;
+                    segs = segs === -1? 0:segs;
 
-                //Insertando el texto en el div del contador
-                timer.innerText = strTime( mins, segs );
+                    //Insertando el texto en el div del contador
+                    timer.innerText = strTime( mins, segs );
 
-            }, 1 );
+                }, 1000 );
 
-        } else {
+            } else {
 
-            //Limpiando el intervalo para la pausa.
-            clearInterval( intervalo );
+                //Limpiando el intervalo para la pausa.
+                clearInterval( intervalo );
 
-            start.innerHTML = '<i class="fas fa-2x fa-play"></i>'; //Cambiando el icono del boton a Play.
+                start.innerHTML = '<i class="btn-start fas fa-2x fa-play"></i>'; //Cambiando el icono del boton a Play.
 
+            }
+
+        } else if( ControlBtn[2] != -1 ) { // Evento boton de añadir minutos
+
+            mins += 10; //Añadiendo 10 minutos al timer.  
+    
+            //Insertando el texto en el div del contador
+            timer.innerText = strTime( mins, segs );
+        
+        } else if( ControlBtn[0] != -1 ) { // Evento boton de quitar minutos
+
+            mins = mins>=5? mins - 5:mins; //Añadiendo 10 minutos al timer.  
+            
+            //Insertando el texto en el div del contador
+            timer.innerText = strTime( mins, segs );
+        
         }
         
     });
 
-    //Evento boton de añadir minutos
-    addMins.addEventListener('click', ()=>{
-
-        mins += 10; //Añadiendo 10 minutos al timer.  
-
-        //Insertando el texto en el div del contador
-        timer.innerText = strTime( mins, segs );
-
-    });
+    
+    
 }
